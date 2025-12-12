d7717b13bd44b63e14ad116f27be5c2cfc72ba09
liza583 <liza30102006@mail.ru>
2025-12-12 22:49:39 +0300

    Create calculate.hpp

---
+++ 3/3.1/calculate.hpp
@@ -0,0 +1,22 @@
+#ifndef CALCULATE_HPP
+#define CALCULATE_HPP
+
+#include <string>
+#include <stdexcept>
+
+struct SensorData {
+    int id;
+    double temperature; // Сохраним последнее значение температуры
+};
+
+// Исключение для пустой строки
+class EmptyInputException : public std::runtime_error {
+public:
+    EmptyInputException() : std::runtime_error("Ошибка: Введена пустая строка или строка состоит только из пробелов.") {}
+};
+
+void calculateTemperatures(const std::string& input, SensorData sensors[], int& sensorCount, bool& hasErrors);
+void sortSensors(SensorData sensors[], int sensorCount, bool sortById);
+
+#endif // CALCULATE_HPP
+
