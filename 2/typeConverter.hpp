9101d8206b2f65b676f86e0d6dd60c6f90e1c8bd
liza583 <liza30102006@mail.ru>
2025-12-12 22:47:57 +0300

    Create typeConverter.hpp

---
+++ 2/typeConverter.hpp
@@ -0,0 +1,11 @@
+#ifndef TYPE_CONVERTER_HPP
+#define TYPE_CONVERTER_HPP
+
+#include <string>
+
+void convertToFloat(const std::string& input);
+void convertToInt(const std::string& input);
+void convertCharToASCII(const std::string& input);
+void convertASCIIToChar(const std::string& input);
+
+#endif // TYPE_CONVERTER_HPP
